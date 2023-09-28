39fbb38b492066f57a0201b8c34f42d150a9d53b
LucasNunes <lucasfnunes6@gmail.com>
2023-09-28 11:37:39 -0300

    Criando assinatura das funções

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,8 +18,51 @@ const state = {
     }
 };
 
+const playerSides = {
+    player1: "player-field-card",
+    player2: "computer-field-card",
+}
+
+const pathImages = ".src/assets/icons/"
+const cardData = [
+    {
+        id:0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: `${pathImages}dragon.png`,
+        WinOf: [1],
+        LoseOf: [2],
+    },
+    {
+        id:1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: `${pathImages}magician.png`,
+        WinOf: [2],
+        LoseOf: [0],
+    },
+    {
+        id:2,
+        name: "Exodia",
+        type: "Scissors",
+        img: `${pathImages}exodia.png`,
+        WinOf: [0],
+        LoseOf: [1],
+    },
+]
+
+function drawCards(cardNumbers, fieldSide) {
+    for (let i = 0; i < cardNumbers; i++) {
+        const randomIdCard = await getRandomCard();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+}
+
 function init() {
-    
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.player2);
 };
 
 init()
